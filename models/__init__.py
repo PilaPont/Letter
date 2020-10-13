ef70eef3bcc0a09d4627d5aa058b69654aadd831
Maryam Kia <maryam.kia1984@mail.com>
2020-10-13 05:41:15 +0000

    Adding Letter module

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import letter
+from . import letterout
+from . import template
+from . import letterin
+
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+from . import common
+from . import test_letter_out
+from . import test_access_right
