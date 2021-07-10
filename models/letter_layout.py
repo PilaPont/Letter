b48a48e8ed4530160121e8f8703607b7edd897ec
Maryam Kia <maryam.kia1984@mail.com>
2021-07-10 16:24:45 +0430

    [IMP] letter,  full page background is sufficient, delete split choice for letter background!

--- models/letter_layout.py
+++ models/letter_layout.py
@@ -11,19 +11,13 @@ class LetterLayout(models.Model):
     background_image = fields.Binary(string="Background Image", attachment=True)
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.user.company_id.id)
     footer_height = fields.Integer('Footer Height (mm)')
-    footer_image = fields.Image(string="Footer Image", attachment=True)
     header_height = fields.Integer('Header Height (mm)')
-    header_image = fields.Image(string="Header Image", attachment=True)
     is_default = fields.Boolean(string="Use as default")
     language_id = fields.Many2one('res.lang', string="language", required=True)
-    layout_type = fields.Selection(selection=[('full', 'Full page'), ('split', 'Different header and Footer')],
-                                   default='full')
-    orientation = fields.Selection([('Landscape', 'Landscape'), ('Portrait', 'Portrait')], default='Landscape')
     page_height = fields.Integer('Page height (mm)', default=False)
     page_size = fields.Selection([(ps['key'], ps['description']) for ps in PAPER_SIZES], 'Paper size', default='A4',
                                  help="Select Proper Paper size")
     page_width = fields.Integer('Page width (mm)', default=False)
-    text_start = fields.Integer('Start text from top (mm)')
 
     @api.onchange('page_size')
     def _onchange_page_size(self):
