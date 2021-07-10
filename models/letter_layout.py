434845721191c85f1f8304e30277c4d0df7dc13a
Maryam Kia <maryam.kia1984@mail.com>
2021-07-10 17:14:16 +0430

    [IMP] letter, full page background is sufficient, delete split choice for letter background!

--- models/letter_layout.py
+++ models/letter_layout.py
@@ -8,24 +8,20 @@ class LetterLayout(models.Model):
     # _inherits = {'report.paperformat': 'paperformat_id'}
 
     name = fields.Char()
-    background_image = fields.Image(string="Background Image", attachment=True)
+    background_image = fields.Binary(string="Background Image", attachment=True)
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.user.company_id.id)
     footer_height = fields.Integer('Footer Height (mm)')
-    footer_image = fields.Image(string="Footer Image", attachment=True)
     header_height = fields.Integer('Header Height (mm)')
-    header_image = fields.Image(string="Header Image", attachment=True)
+    is_default = fields.Boolean(string="Use as default")
     language_id = fields.Many2one('res.lang', string="language", required=True)
-    layout_type = fields.Selection(selection=[('full', 'Full page'), ('split', 'Different header and Footer')],
-                                   default='full')
     page_height = fields.Integer('Page height (mm)', default=False)
     page_size = fields.Selection([(ps['key'], ps['description']) for ps in PAPER_SIZES], 'Paper size', default='A4',
                                  help="Select Proper Paper size")
     page_width = fields.Integer('Page width (mm)', default=False)
-    text_start = fields.Integer('Start text from top (mm)')
 
     @api.onchange('page_size')
     def _onchange_page_size(self):
-        paper_size = list(filter(lambda x: x['key'] == self.page_size, PAPER_SIZES))[0]
-        if paper_size['key'] != 'custom':
+        if self.page_size != 'custom':
+            paper_size = next(ps for ps in PAPER_SIZES if ps['key'] == self.page_size)
             self.page_width = paper_size['width']
             self.page_height = paper_size['height']
