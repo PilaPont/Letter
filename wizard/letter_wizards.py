b0912bc532ab76ca4644088c635c3d38395a84df
Maryam Kia <maryam.kia1984@mail.com>
2021-07-13 13:33:18 +0430

    [Fix] letter, letter id doesn't show in assign wizard, set default for letter_id and edit do_save func.

--- wizard/letter_wizards.py
+++ wizard/letter_wizards.py
@@ -6,11 +6,10 @@ class AssignLetterWizard(models.TransientModel):
     _description = 'Letter Assign Wizard'
 
     letter_id = fields.Many2one(comodel_name='letter.letter',
-                                domain=lambda self: [('type', '=', 'in')], required=True,
+                                default=lambda self: self._context.get('active_id'), required=True,
                                 readonly=True)
     subject = fields.Char(related='letter_id.subject', string='Subject', readonly=True)
     user_id = fields.Many2one('res.users', string='Assignee', required=True)
 
     def do_save(self):
-        letter = self.env['letter.letter'].browse(self.letter_id.id)  # (self.env.context.get('active_ids'))
-        letter.write({'user_id': self.user_id.id, 'state': 'to_do'})
+        self.letter_id.write({'user_id': self.user_id.id, 'state': 'to_do'})
